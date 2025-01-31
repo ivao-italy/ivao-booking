12608bf88ae2a5122d8e342879f7733a9a006122
Innocenti Emiliano <emiliano@emilianoinnocenti.com>
2025-01-31 09:02:34 +0100

    Favicon

--- index.php
+++ index.php
@@ -91,6 +91,44 @@ Menu::addItems([
 	<script>
 		const XSRF_TOKEN = "<?=$_SESSION["xsrfToken"]; ?>";
 	</script>
+
+
+ 	<!--Icons-->
+ 	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="favicon/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="favicon/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="favicon/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="favicon/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="favicon/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="favicon/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="favicon/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="favicon/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="favicon/favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="favicon/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="favicon/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="favicon/favicon-128.png" sizes="128x128" />
+    <meta name="application-name" content="IVAO IT Booking System" />
+    <meta name="msapplication-TileColor" content="#0D2C99" />
+    <meta name="msapplication-TileImage" content="favicon/mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="favicon/mstile-70x70.png" />
+    <meta name="msapplication-square150x150logo" content="favicon/mstile-150x150.png" />
+    <meta name="msapplication-wide310x150logo" content="favicon/mstile-310x150.png" />
+    <meta name="msapplication-square310x310logo" content="favicon/mstile-310x310.png" />
+
+
+	<!--open graph meta tags for social sites and search engines-->
+    <meta property="og:locale" content="en-US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="IVAO Italy - Booking System" />
+    <meta property="og:description" content="Booking system of IVAO Italy for the event: <?=$config["event_name"]; ?>" />
+    <meta property="og:url" content="https://it.ivao.aero" />
+    <meta property="og:site_name" content="IVAO Italy Booking System" />
+    <meta property="og:image" content="favicon/favicon-196x196.png" />
+    <meta property="og:image:secure_url" content="favicon/favicon-196x196.png" />
+    <meta property="og:image:width" content="196" />
+    <meta property="og:image:height" content="196" />
+
+    <meta name="theme-color" content="#0D2C99">
 </head>
 
 <body>
